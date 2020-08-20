596a8787ffc99bbaf0b81596a7106379b879d049
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-08-20 14:40:16 +0100

    tutorial

--- AppToPhone/ViewController.m
+++ AppToPhone/ViewController.m
@@ -7,16 +7,95 @@
 //
 
 #import "ViewController.h"
+#import <NexmoClient/NexmoClient.h>
 
-@interface ViewController ()
-
+@interface ViewController () <NXMClientDelegate>
+@property UIButton *callButton;
+@property UILabel *connectionStatusLabel;
+@property NXMClient *client;
+@property NXMCall * call;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.connectionStatusLabel = [[UILabel alloc] init];
+    self.connectionStatusLabel.text = @"Unknown";
+    self.connectionStatusLabel.textAlignment = NSTextAlignmentCenter;
+    self.connectionStatusLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.connectionStatusLabel];
+    
+    self.callButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.callButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.callButton setAlpha:0];
+    [self.callButton addTarget:self action:@selector(callButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    [self.callButton setTitle:@"Call" forState:UIControlStateNormal];
+    [self.view addSubview:self.callButton];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.connectionStatusLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20],
+        [self.connectionStatusLabel.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.connectionStatusLabel.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        
+        [self.callButton.topAnchor constraintEqualToAnchor:self.connectionStatusLabel.bottomAnchor constant:40],
+        [self.callButton.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.callButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20]
+    ]];
+    
+    self.client = NXMClient.shared;
+    [self.client setDelegate:self];
+    [self.client loginWithAuthToken:@"ALICE_JWT"];
+}
+
+- (void)callButtonPressed {
+    if (!self.call) {
+        [self placeCall];
+    } else {
+        [self endCall];
+    }
+}
+
+- (void)placeCall {
+    [self.client call:@"PHONE_NUMBER" callHandler:NXMCallHandlerServer completionHandler:^(NSError * _Nullable error, NXMCall * _Nullable call) {
+        if (error) {
+            self.connectionStatusLabel.text = error.localizedDescription;
+            return;
+        }
+        
+        self.call = call;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.callButton setTitle:@"End call" forState:UIControlStateNormal];
+        });
+    }];
+}
+
+- (void)endCall {
+    [self.call hangup];
+    self.call = nil;
+    [self.callButton setTitle:@"Call" forState:UIControlStateNormal];
+}
+
+- (void)client:(nonnull NXMClient *)client didChangeConnectionStatus:(NXMConnectionStatus)status reason:(NXMConnectionStatusReason)reason {
+    switch (status) {
+        case NXMConnectionStatusConnected:
+            [self.callButton setAlpha:1];
+            self.connectionStatusLabel.text = @"Connected";
+            break;
+        case NXMConnectionStatusConnecting:
+            self.connectionStatusLabel.text = @"Connecting";
+            break;
+        case NXMConnectionStatusDisconnected:
+            self.connectionStatusLabel.text = @"Disconnected";
+            break;
+    }
+}
+
+- (void)client:(nonnull NXMClient *)client didReceiveError:(nonnull NSError *)error {
+    self.connectionStatusLabel.text = error.localizedDescription;
+    [self.callButton setAlpha:0];
 }
 
 
